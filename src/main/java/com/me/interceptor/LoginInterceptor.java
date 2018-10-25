8ebd91ba884a06df22790598d210b99b2cf751f0
HolyDogs <525443029@qq.com>
2018-10-26 00:53:15 +0800

    增加登陆验证的拦截器功能

---
+++ src/main/java/com/me/interceptor/LoginInterceptor.java
@@ -0,0 +1,44 @@
+package com.me.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.me.pojo.User;
+
+public class LoginInterceptor implements HandlerInterceptor {
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		String url=request.getRequestURI();
+		if (url.indexOf("login")>0||url.indexOf("come")>0) {
+			return true;
+		}
+		
+		User user=(User) request.getSession().getAttribute("user");
+		if (user!= null) {
+			return true;
+		}
+		
+		response.sendRedirect("login.jsp");
+		return false;
+	}
+
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+			ModelAndView modelAndView) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
+			throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+}
